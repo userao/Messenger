109f9d67bf6081bb0edfb60f9d967225f89f93d1
userao <ostapenko2121@gmail.com>
2022-10-26 15:32:54 +0600

    update login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Button from 'react-bootstrap/Button';
 import { useFormik } from 'formik';
 import axios from 'axios';
@@ -62,48 +61,43 @@ const LoginForm = () => {
             <div className="card-body d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
               <Form className="w-50" onSubmit={formik.handleSubmit}>
                 <h1 className="text-center mb-4">{t('header')}</h1>
-                <Form.Group className="mb-3">
-                  <FloatingLabel
-                    className="mb-3"
-                    label={t('usernameLabel')}
-                    htmlFor="username"
-                  >
-                    <Form.Control
-                      ref={usernameInput}
-                      required
-                      id="username"
-                      name="username"
-                      type="username"
-                      onChange={formik.handleChange}
-                      value={formik.values.username}
-                      className={loginState === 'incorrect data' ? 'is-invalid' : null}
-                      disabled={loginState === 'requesting'}
-                    />
-                  </FloatingLabel>
-                </Form.Group>
+                <Form.Floating
+                  className="mb-3"
+                >
+                  <Form.Control
+                    ref={usernameInput}
+                    required
+                    id="username"
+                    name="username"
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
+                    placeholder={t('usernameLabel')}
+                    className={loginState === 'incorrect data' ? 'is-invalid' : null}
+                    disabled={loginState === 'requesting'}
+                  />
+                  <label className="form-label" htmlFor="username">{t('usernameLabel')}</label>
+                </Form.Floating>
 
-                <Form.Group className="mb-3">
-                  <FloatingLabel
-                    className="mb-3"
-                    label={t('passwordLabel')}
-                    htmlFor="password"
-                  >
-                    <Form.Control
-                      required
-                      id="password"
-                      name="password"
-                      type="password"
-                      onChange={formik.handleChange}
-                      value={formik.values.password}
-                      className={loginState === 'incorrect data' ? 'is-invalid' : null}
-                      disabled={loginState === 'requesting'}
-                    />
-                    {loginState === 'success' && navigate(redirectPath)}
-                    {loginState === 'incorrect data'
-                      ? <div className="invalid-tooltip">{t('invalidTooltip')}</div>
-                      : null}
-                  </FloatingLabel>
-                </Form.Group>
+                <Form.Floating
+                  className="mb-4"
+                >
+                  <Form.Control
+                    required
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder={t('passwordLabel')}
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    className={loginState === 'incorrect data' ? 'is-invalid' : null}
+                    disabled={loginState === 'requesting'}
+                  />
+                  <label className="form-label" htmlFor="password">{t('passwordLabel')}</label>
+                  {loginState === 'success' && navigate(redirectPath)}
+                  {loginState === 'incorrect data'
+                    ? <div className="invalid-tooltip">{t('invalidTooltip')}</div>
+                    : null}
+                </Form.Floating>
                 <Button className="w-100" disabled={loginState === 'requesting'} variant="primary" type="submit">{t('logInButton')}</Button>
               </Form>
             </div>
